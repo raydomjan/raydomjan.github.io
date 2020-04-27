e2a9d2d2e34c780025557ae213e16f4e65499566
raydomjan <rdomjan@umd.edu>
2020-04-26 23:05:41 -0400

    lab10

---
+++ lab_10/server.js
@@ -0,0 +1,75 @@
+
+
+import express from "express";
+import fetch from "node-fetch";
+
+
+
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import writeUser from "./libraries/writeuser.js";
+
+const dbSettings = {
+  filename: "./tmp/database.db",
+  driver: sqlite3.Database,
+};
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+
+
+
+
+
+function processDataForFrontEnd(req, res) {
+  const baseURL = ""; 
+  fetch(baseURL)
+    .then((r) => r.json())
+    .then((data) => {
+      console.log(data);
+      res.send({ data: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/error");
+    });
+}
+
+
+app
+  .route("/api")
+  .get((req, res) => {
+   
+    (async () => {
+      const db = await open(dbSettings);
+      const result = await db.all("SELECT * FROM user");
+      console.log("Expected result", result);
+      res.json(result);
+    })();
+  })
+  .put((req, res) => {
+    console.log("/api put request", req.body);
+    if (!req.body.name) {
+      console.log(req.body);
+      res.status("418").send("something went wrong, additionally i am a teapot");
+    } else {
+      writeUser(req.body.name, dbSettings)
+      .then((result) => {
+        console.log(result);
+        res.send("good job");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
